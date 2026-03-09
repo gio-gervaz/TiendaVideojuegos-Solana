e01be541e0999155a8bd3d27ff4b1c3a3ef74678
gio-gervaz <geovaspablo9@gmail.com>
2026-03-09 11:00:42 -0600

    Add initial test for tienda_videojuegos

--- tests/tienda_videojuegos.ts
+++ tests/tienda_videojuegos.ts
@@ -1,1 +1,7 @@
+import * as anchor from "@coral-xyz/anchor";
 
+describe("tienda-videojuegos", () => {
+  it("Test inicial", async () => {
+    console.log("Proyecto tienda de videojuegos funcionando");
+  });
+});
