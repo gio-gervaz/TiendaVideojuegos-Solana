946520c6860c70daca5fbf0caf9cf98ae820f21f
gio-gervaz <geovaspablo9@gmail.com>
2026-03-09 11:13:34 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,1 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
 
+async function main() {
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  console.log("Conectado a Solana");
+
+  const program = anchor.workspace.TiendaVideojuegos;
+
+  console.log("Programa cargado:", program.programId.toString());
+
+}
+
+main().catch(console.error);
